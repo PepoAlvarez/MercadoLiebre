82e5bab0f1e0e8a8e63540539ef95c5608be7548
PepoAlvarez <alv.jl@hotmail.com>
2021-09-16 22:02:41 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require("path")
+
+
+app.listen(process.env.PORT || 3000, function(){
+    ('anda todo bien en http://localhost:3000');
+})
+
+
+app.get('/',function(req,res){res.sendFile(path.join(__dirname,"/views/home.html"))})
+app.get('/login',function(req,res){res.sendFile(path.join(__dirname,"/views/login.html"))})
+app.get('/register',function(req,res){res.sendFile(path.join(__dirname,"/views/register.html"))})
+
+
+
+app.use(express.static("public"))
